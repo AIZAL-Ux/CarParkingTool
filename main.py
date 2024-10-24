a69b9631933c773a7e004b5355ebfb25b461d48e
AIZAL-Baloch <aizal.nuker.ly@gmail.com>
2024-10-24 16:42:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmgala import CPMGala
+from carparktool import CarParkTool
 
 __CHANNEL_USERNAME__ = "CP_Aizal"
 __GROUP_USERNAME__   = "CP_Aizal_chat"
